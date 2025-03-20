3d7ee9b40dcd1001e87db209bd770e377a929684
leooli0 <leonardosantos300305@gmail.com>
2025-03-20 19:06:50 -0300

    first commmit"

---
+++ aulaFaculdade-linguagemC.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+/*
+
+    Aula de introducao a linguagem C, nessa aula foi feito o primeiro programa
+    na linguagem C.
+
+*/
+int main() {
+
+    printf("Hello World!\n");
+    return 0;
+
+}
